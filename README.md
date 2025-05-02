8216e437044239573d996aeb5b7a266553c3a5df
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-05-02 14:16:48 +0200

    Project Structure

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# freshguard
-FreshGuard helps you to keep your home products fresh under the expiration date
+# Fresh Guard
+Fresh Guard is a simple and easy to use tool that helps you keep your pantry fresh by reminding you to use your food
+before it goes bad.
+
+It a mobile first web application that allows you to add items to your pantry, set expiration dates, and receive
+notifications when items are about to expire.
+
+# Installation
+To install Fresh Guard, follow these steps:
+1. Clone the repository:
+   ```bash
+   git clone git@github.com:marcelorodrigo/freshguard.git
+    ```
+2. Run the following command to install the dependencies:
+   ```bash
+   composer install
+   npm install
+   ```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fresh Guard</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body>
+    <h1 class="flex items-center text-2xl font-semibold text-green-600 text-shadow-xs">
+        <img src="/logo.svg" class="w-10" alt="Fresh Guard">
+        Fresh Guard
+    </h1>
+</body>
+</html>
