0dbfae2f58b85c8f8664c65d605ee466cb432268
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-12-26 17:39:45 +0100

    fix: Update visibility condition for quantity field in ItemForm

--- app/Filament/Resources/Items/Schemas/ItemForm.php
+++ app/Filament/Resources/Items/Schemas/ItemForm.php
@@ -37,7 +37,7 @@ class ItemForm
                     ->readOnly()
                     ->helperText(__('The quantity is computed from all batches'))
                     ->label(__('Quantity'))
-                    ->hidden(static fn ($get, $record) => is_null($record)),
+                    ->hidden(static fn ($record) => is_null($record)),
                 TextInput::make('expiration_notify_days')
                     ->integer()
                     ->suffix(__('days'))
