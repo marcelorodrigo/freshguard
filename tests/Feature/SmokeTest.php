395f40f8791f652a1cbaa63af14e71e0c43b5523
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-12-29 16:42:47 +0100

    fix: Remove redundant test for admin status modification in UserResourceTest

--- tests/Feature/SmokeTest.php
+++ tests/Feature/SmokeTest.php
@@ -2,12 +2,8 @@
 
 namespace Tests\Feature;
 
-it('registration is enabled when FRESHGUARD_REGISTRATIONS_ENABLED config is true', function () {
-    expect(config('freshguard.registrations_enabled'))->toBeTrue();
-});
-
-it('registration can be disabled when FRESHGUARD_REGISTRATIONS_ENABLED config is false', function () {
-    config()->set('freshguard.registrations_enabled', false);
-
-    expect(config('freshguard.registrations_enabled'))->toBeFalse();
+it('It redirects to the login page', function () {
+    $response = $this->get('/');
+    $response->assertStatus(302)
+        ->assertRedirect("/login");
 });
