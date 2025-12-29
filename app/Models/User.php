1985669a1b8fd8681162cfd25866e4cddd14ef49
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-12-29 11:48:49 +0100

    feat: implement MustVerifyEmail interface in User model for email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable implements FilamentUser, MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
