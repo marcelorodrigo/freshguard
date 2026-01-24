b82862a58012707f2183bd2ddf821017bb599b35
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-01-24 10:06:03 +0100

    refactor(model): remove unused BelongsTo import from Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
