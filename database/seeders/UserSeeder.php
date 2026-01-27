f0b73ea15aa48b2888d3544ec7ed21ea2cdd1c47
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-01-27 17:27:42 +0100

    chore(seeder): use plain password for test user in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +17,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test at Example.com',
             'email' => 'test@example.com',
-            'password' => Hash::make('test@example.com'),
+            'password' => 'test@example.com',
             'email_verified_at' => now(),
         ]);
     }
