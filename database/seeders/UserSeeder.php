23341b8bf0a5527cef9222d32aff4180462861b8
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-02-22 21:24:49 +0100

    fix: hash password in UserSeeder for improved security

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test at Example.com',
             'email' => 'test@example.com',
-            'password' => 'test@example.com',
+            'password' => Hash::make('test@example.com'),
             'email_verified_at' => now(),
         ]);
     }
